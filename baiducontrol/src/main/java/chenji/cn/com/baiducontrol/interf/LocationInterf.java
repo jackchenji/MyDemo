f00443ad704c8f328ac775541e5f7928cba250b7
chenji@f-road.com.cn <chenji@f-road.com.cn>
2018-11-29 18:03:17 +0800

    框架搭建

--- baiducontrol/src/main/java/chenji/cn/com/baiducontrol/interf/LocationInterf.java
+++
@@ -1,9 +0,0 @@
-package chenji.cn.com.baiducontrol.interf;
-
-/**
- * author by chenji on 2018/11/26
- */
-public interface LocationInterf {
-    void onLocationSuccess(String location);
-    void onLocationFail(String codeInfo);
-}
